b1210f3e3fa8d2245a46a62d91e1cffb348c27d4
Erick Campos <kcire2477@gmail.com>
2022-03-12 19:01:08 -0600

    Ultima función

--- js/app.js
+++ js/app.js
@@ -43,6 +43,7 @@ function consultarAPI(ciudad, pais){
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`;
 
+    
     fetch(url)
         .then(resolve => resolve.json())
         .then(datos => {
@@ -99,4 +100,25 @@ function limpiarHTML(){
     while(resultado.firstChild){
         resultado.removeChild(resultado.firstChild);
     }
+}
+function spinner(){
+    console.log('spinner');
+    limpiarHTML();
+    const divSpinner = document.createElement('DIV') ;
+    divSpinner.classList.add('.sk-fading-circle');
+    divSpinner.innerHTML = `
+        <div class="sk-circle1 sk-circle"></div>
+        <div class="sk-circle2 sk-circle"></div>
+        <div class="sk-circle3 sk-circle"></div>
+        <div class="sk-circle4 sk-circle"></div>
+        <div class="sk-circle5 sk-circle"></div>
+        <div class="sk-circle6 sk-circle"></div>
+        <div class="sk-circle7 sk-circle"></div>
+        <div class="sk-circle8 sk-circle"></div>
+        <div class="sk-circle9 sk-circle"></div>
+        <div class="sk-circle10 sk-circle"></div>
+        <div class="sk-circle11 sk-circle"></div>
+        <div class="sk-circle12 sk-circle"></div>
+    `;
+    resultado.appendChild(divSpinner);
 }
